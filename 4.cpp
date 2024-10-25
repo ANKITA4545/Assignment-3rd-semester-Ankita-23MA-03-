5d0b136b8f0394f799cf309a298338d9b6e89ce5
ANKITA4545 <ankitanegii822@gmail.com>
2024-10-25 06:57:16 -0700

    Create 4.cpp

---
+++ 4.cpp
@@ -0,0 +1,28 @@
+// display sum of two integer value sum of two float value two char value using constructor overloading
+#include <iostream
+#include<string>
+using namespace std;
+
+class A{
+    public:
+        A(int x,int y)
+        {
+            cout<<"sum : "<<x+y<<endl;
+        }
+        A(double x,double y)
+        {
+            cout<<"sum : "<<x+y<<endl;
+        }
+        A(string x,string y)
+        {
+            cout<<"sum : "<<x+y<<endl;
+        }
+};
+int main() {
+    
+    A oa1(5,8);
+    A oa2(5.6,2.4);
+    A oa3("c","++");
+
+    return 0;
+}
